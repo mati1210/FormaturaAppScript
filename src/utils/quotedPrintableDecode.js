d9d8acae1286f120cefa009543cf5159412428af
mat1210 <matheuz1210@gmail.com>
2025-06-08 01:44:58 -0300

    quotedPrintableDecode: rename

--- src/utils/quotedPrintableDecode.js
+++ src/utils/quotedPrintableDecode.js
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-function quoted_printable_decode(str: string): string {
+function quotedPrintableDecode(str: string): string {
     return str.replace(/(=[A-Z0-9]{2})+/g, char => {
         let bytearr = new Uint8Array(char.slice(1).split('=').map(b => Number.parseInt(b, 16)));
         let decoder = new TextDecoder('utf8');
